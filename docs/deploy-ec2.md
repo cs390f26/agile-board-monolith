077662819f38f9b035b22f922f04e43eed9c8cb1
Jamell Alvarez <alvarezj@moravian.edu>
2026-09-19 20:33:27 -0400

    Added barebones setup for app, deploy, and docs

---
+++ docs/deploy-ec2.md
@@ -0,0 +1,3 @@
+Deploy on ec2
+--------------
+This document explains how to run the monolithic voting app on an EC2 instance.
---
+++ src/agile-board/app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+
+def create_app():
+    app = Flask(__name__)
+
+    @app.route('/')
+    def index_page():
+        return render_template('index.html')
+
+    @app.route('/manager')
+    def manager_view():
+        return render_template('manager.html')
+
+    @app.route('/engineer/<name>')
+    def engineer_view(name):
+        return render_template('engineer.html', name=name)
+
+    return app
+
+
+print("hello world")
