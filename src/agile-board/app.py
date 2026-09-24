02dffbfbfb401859405e82f8b721dd05524fa944
Jamell Alvarez <alvarezj@moravian.edu>
2026-09-24 18:22:07 -0400

    Adjusted the api endpoints in the documentation and app.py, adjusted the layout for manager page, added contributors to readme

--- src/agile-board/app.py
+++ src/agile-board/app.py
@@ -16,4 +16,28 @@ def create_app():
     def engineer_view(name):
         return render_template('engineer.html', name=name)
 
+    @app.route('/api/summary_json', methods = (['GET']))
+    def whole_summary():
+        return {'name': 'John Doe', 'tasks': ['Task 1', 'Task 2']}
+
+    @app.route('api/task/', methods = (['POST']))
+    def create_task():
+        return {'name': 'John Doe', 'tasks': ['Task 1', 'Task 2']}
+
+    @app.route('/api/task/<task_Id>/assign', methods = (['POST']))
+    def assign_task():
+        return {'name': 'John Doe', 'tasks': ['Task 1', 'Task 2']}
+
+    @app.route('/api/task/<task_Id>/<person_Id>/inprogress', methods = (['POST']))
+    def update_task_in_progress():
+        return {'name': 'John Doe', 'tasks': ['Task 1', 'Task 2']}
+
+    @app.route('/api/task/<task_Id>/<person_Id>/done', methods = (['POST']))
+    def update_task_done():
+        return {'name': 'John Doe', 'tasks': ['Task 1', 'Task 2']}
+
+    @app.route('/api/people/<people_Id>', methods = (['POST']))
+    def create_person():
+        return {'name': 'John Doe', 'tasks': ['Task 1', 'Task 2']}
+
     return app
